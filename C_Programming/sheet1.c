295b41989c453a4731061e89131658d1ede81740
a01014181179s-crypto <a01014181179s@gmail.com>
2026-09-10 03:30:39 +0300

    HomeWork1

---
+++ C_Programming/sheet1.c
@@ -0,0 +1,96 @@
+/*---HomeWork1---
+Name: Ali Ahmed Hassan Sawan
+*/
+
+#include <stdio.h>
+
+//--Assinment at Pdf--
+
+//#include <stdio.h>
+//
+//int main() {
+//	int i,j;
+//    for ( i = 0; i <= 9; i++) {
+//        for ( j = i; j <= 9; j++) {
+//            printf("%d ", j);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+
+//---Ex.1------
+
+//void main()
+//{
+//	printf("C Programming");
+//}
+
+//---Ex.2------
+
+//int i;
+//void main()
+//{
+//	printf("Enter a integer:");
+//	fflush(stdout);
+//	scanf("%d",&i);
+//	printf("You Entered: %d",i);
+//}
+
+//---Ex.3------
+
+//int i,j;
+//void main()
+//{
+//		printf("Enter a integer:");
+//		fflush(stdin);fflush(stdout);
+//		scanf("%d %d",&i,&j);
+//		printf("Sum: %d\n", i + j);
+//}
+
+//---Ex.4------
+
+//float i,j;
+//void main()
+//{
+//		printf("Enter a integer:");
+//		fflush(stdin);fflush(stdout);
+//		scanf("%f %f",&i,&j);
+//		printf("Products: %f\n", i * j);
+//}
+
+//---Ex.5------
+
+//char i;
+//void main()
+//{
+//		printf("Enter a character:");
+//		fflush(stdin);fflush(stdout);
+//		scanf("%c",&i);
+//		printf("ASCII value of %c = %d",i,i);
+//}
+
+//---Ex.6------
+
+float a,b,New;
+void main()
+{
+	printf("Enter a value of a:");
+	fflush(stdin);fflush(stdout);
+	scanf("%f",&a);
+	printf("Enter a value of b:");
+	fflush(stdin);fflush(stdout);
+	scanf("%f",&b);
+	New = a;      //Make a in variable because it removed when is changing
+	a = b;        //New a value = b
+	b = New;      //Take kept value from New
+	printf("The value of a: %f",a);
+	printf("\nThe value of b: %f",b);
+}
+
+
+
+
+
+
+
