bcbe0e91aaa3f2c6baf3035b2f9e3ab8daba574f
a01014181179s-crypto <a01014181179s@gmail.com>
2026-09-21 02:34:18 +0300

    Homework2

---
+++ C_Programming/sheet2.c
@@ -0,0 +1,193 @@
+/*---HomeWork2---
+Name: Ali Ahmed Hassan Sawan
+*/
+
+#include "stdio.h"
+
+//---Ex.1------
+
+//int a;
+//void main()
+//{
+//	printf("Enter an integer:");
+//	fflush(stdin);fflush(stdout);
+//	scanf("%d",&a);
+//	if((a%2)==0)
+//	{
+//		printf("%d is Even",a);
+//	}
+//	else
+//	{
+//		printf("%d is Odd",a);
+//	}
+//}
+
+//---Ex.2------
+
+//char c;
+//void main()
+//{
+//	printf("Enter an Alphabet:");
+//	fflush(stdin);fflush(stdout);
+//	scanf("%c",&c);
+//	if(c=='a'||c=='A'||c=='e'||c=='E'||c=='i'||c=='I'||c=='o'||c=='O')
+//	{
+//		printf("%c is a vowel",c);
+//	}
+//	else
+//	{
+//		printf("%c is a consonant",c);
+//	}
+//}
+
+//---Ex.3------
+
+//float a,b,c;
+//void main()
+//{
+//	printf("Enter Three Numbers:");
+//	fflush(stdin);fflush(stdout);
+//	scanf("%f \n%f \n%f",&a,&b,&c);
+//	if(a>b&&a>c)
+//	{
+//        printf("%.2f is the Largest Number",a);
+//	}
+//	else if(b>a&&b>c)
+//	{
+//		printf("%.2f is the Largest Number",b);
+//	}
+//	else if(c>a&&c>b)
+//	{
+//		printf("%.2f is the Largest Number",c);
+//	}
+//	else
+//	{
+//		printf("Error");
+//	}
+//}
+
+//---Ex.4------
+
+//float a;
+//void main()
+//{
+//	printf("Enter The Number:");
+//	fflush(stdin);fflush(stdout);
+//	scanf("%f",&a);
+//	if(a>0)
+//	{
+//        printf("%.2f is Positive",a);
+//	}
+//	else if(a==0)
+//	{
+//		printf("%.2f is Zero",a);
+//	}
+//	else if(a<0)
+//	{
+//		printf("%.2f is Negative",a);
+//	}
+//	else
+//	{
+//		printf("That is not number");
+//	}
+//}
+
+//---Ex.5------
+
+//char c;
+//void main()
+//{
+//	printf("Enter an Alphabet:");
+//	fflush(stdin);fflush(stdout);
+//	scanf("%c",&c);
+//	if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') )
+//	{
+//		printf("%c is an alphabet",c);
+//	}
+//	else
+//	{
+//		printf("%c is not an alphabet",c);
+//	}
+//}
+
+//---Ex.6------
+//
+//int n,count,sum;
+//void main()
+//{
+//	printf("Enter an integer:");
+//	fflush(stdin);fflush(stdout);
+//	scanf("%d",&n);
+//	for(count=1;count<=n;count++)
+//	{
+//		sum+=count;
+//	}
+//	printf("\nSum = %d",sum);
+//}
+
+//---Ex.7------
+
+//long long n,count,factorial=1;
+//void main()
+//{
+//	printf("Enter an integer:");
+//	fflush(stdin);fflush(stdout);
+//	scanf("%lld",&n);
+//	if(n>=0)
+//	{
+//		for(count=1;count<=n;count++)
+//		{
+//			factorial *=count;
+//		}
+//		printf("\nfactorial = %lld",factorial);
+//	}
+//	else
+//	{
+//		printf("\nError = %lld",n);
+//	}
+//}
+
+//---Ex.8------
+
+//float a,b;
+//char x;
+//void main()
+//{
+//	printf("Enter two integet:");
+//	fflush(stdin);fflush(stdout);
+//	scanf("%f %f",&a,&b);
+//	printf("Enter The Arthmetic Operation:");
+//	fflush(stdin);fflush(stdout);
+//	scanf(" %c",&x);
+//	switch(x){
+//	case '+' :
+//		printf("%f + %f = %f",a,b,a + b);
+//		break;
+//	case '-' :
+//		printf("%f - %f = %f",a,b,a - b);
+//		break;
+//	case '*' :
+//		printf("%f * %f = %f",a,b,a * b);
+//		break;
+//	case '/' :
+//		printf("%f / %f = %f",a,b,a / b);
+//		break;
+//	default:
+//		printf("The Operation is not correct\n");
+//		break;
+//	}
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
